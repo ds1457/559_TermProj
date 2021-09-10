a276768725399bbbcc2bd430fa0ad82051dd7f7c
ds1457 <90416836+ds1457@users.noreply.github.com>
2021-09-10 01:53:00 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -67,7 +67,7 @@ app.post('/api/articles/:name/add-comment', (req, res) => {
 
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + './build/index.html'));
+    res.sendFile(path.join(__dirname + '/build/index.html'));
 })
 
 
